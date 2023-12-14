e3757c7a24f4cded184a1665739aca2ab4286d97
colleym-cloud <colley23m@gmail.com>
2023-12-14 12:25:59 +0000

    111

--- web_app/app.py
+++ web_app/app.py
@@ -1,16 +1,45 @@
  # app.py
 from flask import Flask, render_template
-import paho.mqtt.subscribe as subscribe
+import RPi.GPIO as GPIO
+import time
+from gunicorn_config import bind, workers
+
+
 
 app = Flask(__name__)
 
+TRIG = 17  # GPIO17
+ECHO = 18  # GPIO18
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(TRIG, GPIO.OUT)
+GPIO.setup(ECHO, GPIO.IN)
+
+def get_distance():
+    GPIO.output(TRIG, True)
+    time.sleep(0.00001)
+    GPIO.output(TRIG, False)
+
+    pulse_start = time.time()
+    pulse_end = time.time()
+
+    while GPIO.input(ECHO) == 0:
+        pulse_start = time.time()
+
+    while GPIO.input(ECHO) == 1:
+        pulse_end = time.time()
+
+    pulse_duration = pulse_end - pulse_start
+    distance = pulse_duration * 17150  # Speed of sound is 343m/s, and there and back.
+    distance = round(distance, 2)
+
+    return distance
+
 @app.route('/')
 def index():
-    return render_template('index.html', status=get_sensor_status())
-
-def get_sensor_status():
-    msg = subscribe.simple("parking/status", hostname="localhost")
-    return msg.payload.decode('utf-8')
+    distance = get_distance()
+    status = "Occupied" if distance < 10 else "Free"
+    return render_template('index.html', distance=distance, status=status)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(debug=True, host='0.0.0.0', port=5001)
